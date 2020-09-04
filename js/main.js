ea4a930c576690de66f88f5779275b563868aa97
Jogendra <jogendrafx@gmail.com>
2020-09-05 05:02:24 +0530

    Completed main screen totally

--- js/main.js
+++ js/main.js
@@ -41,7 +41,21 @@ let recData = JSON.parse(recJSON);
 recCard.innerHTML = generateVideoCardsHTML(recData, "card-1");
 
 // Populate Popular section data
+// Trending
 let trendingJSON = '{"items":[{"img":"http:\/\/i3.ytimg.com\/vi\/8ui9umU0C2g\/maxresdefault.jpg","title":"The Silence","channel":"Manchester Orchestra ✓","time":"1.5 years","views":28192126},{"img":"http:\/\/i3.ytimg.com\/vi\/tXsQJhoauxc\/maxresdefault.jpg","title":"The Ballad Of Cleopatra","channel":"The Lumineers","time":"3 months","views":981392},{"img":"http:\/\/i3.ytimg.com\/vi\/CjUedl3HsRs\/maxresdefault.jpg","title":"Welcome to Slow Air","channel":"Still Corners","time":"2 year","views":8949659},{"img":"http:\/\/i3.ytimg.com\/vi\/PvJoBqrMRm8\/maxresdefault.jpg","title":"Lost Tapes | Chill Mix Vol.2","channel":"Alex Aero ✓","time":"11 months","views":78933}]}';
 let trendingCard = document.getElementById('trendingCard');
 let trendingData = JSON.parse(trendingJSON);
 trendingCard.innerHTML = generateVideoCardsHTML(trendingData, "card-4")
+// Music
+let musicJSON = '{"items":[{"img":"http:\/\/i3.ytimg.com\/vi\/PVjiKRfKpPI\/maxresdefault.jpg","title":"Hozier - Take Me To Church (Official Video)","channel":"Hozier \u2713","time":"5 years","views":372189},{"img":"http:\/\/i3.ytimg.com\/vi\/wepBAVq_3jQ\/maxresdefault.jpg","title":"Time Will Tell by Gregory Alan Isakov","channel":" Red Barn Studio Live Sessions","time":"3 days","views":56281},{"img":"http:\/\/i3.ytimg.com\/vi\/EWLqdAJbu0A\/maxresdefault.jpg","title":"Hozier - Shrike (Official Audio)","channel":"Hozier \u2713","time":"7 months","views":547662},{"img":"http:\/\/i3.ytimg.com\/vi\/0AYzzzBaPBI\/maxresdefault.jpg","title":"They Own This Town","channel":"Flora Cash \u2713","time":"yesterday","views":83062}]}';
+let musicCard = document.getElementById('musicCard');
+let musicData = JSON.parse(musicJSON);
+musicCard.innerHTML = generateVideoCardsHTML(musicData, "card-4")
+// Gaming
+let gamingCard = document.getElementById('gamingCard');
+let gamingJSON = '{"items":[{"img":"http:\/\/i3.ytimg.com\/vi\/Kuy17LO14OA\/maxresdefault.jpg","title":"1-Hour Music Mix | Video Game Soundtracks","channel":"Pandora Journey","time":"2 days","views":89357},{"img":"http:\/\/i3.ytimg.com\/vi\/jDvclMjIttQ\/maxresdefault.jpg","title":"Hostel Games Part 1 - TSPs Bade Chote","channel":"The Screen Patti ✓","time":"1 year","views":28349659},{"img":"http:\/\/i3.ytimg.com\/vi\/db97HUw3ToA\/maxresdefault.jpg","title":"Microsoft Flight Simulator Review","channel":"IGN ✓","time":"2 weeks","views":97351392},{"img":"http:\/\/i3.ytimg.com\/vi\/YOFOb_FJwyI\/maxresdefault.jpg","title":"Dreamliner Pilot Plays New Microsoft Flight Simulator","channel":"AviatorDan","time":"11 days","views":783386}]}';
+let gamingData = JSON.parse(gamingJSON);
+gamingCard.innerHTML = generateVideoCardsHTML(gamingData, "card-4");
+// Extra - to show scrolling
+let sbsCard = document.getElementById('sbsCard');
+sbsCard.innerHTML = generateVideoCardsHTML(subscriptionData, "card-4");
