10d9b6993e0ff5a0e4c74d4ccb609f8bbe5ecc82
Jogendra <jogendrafx@gmail.com>
2020-09-04 23:55:29 +0530

    Add selected state behaviour to side bar items

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// Chage Selected Item in Side Bar
+let topItems = document.getElementById("topItems");
+let sideItems = topItems.getElementsByClassName("side-item");
+for (let i = 0; i < sideItems.length; i++) {
+    sideItems[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active-item");
+    if (current.length > 0) { 
+        current[0].className = current[0].className.replace(" active-item", "");
+    }
+    this.className += " active-item";
+  });
+}
